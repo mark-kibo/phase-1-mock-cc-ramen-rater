fe86a55cf938673386f68b440b71788dae0a3e87
mark-kibo <kibochamark@gmail.com>
2023-07-23 11:18:40 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,72 @@
 // write your code here
+let ramenDiv= document.getElementById("ramen-menu")
+let imageName= document.querySelector("#ramen-detail h2")
+let imageRestaurant= document.querySelector("#ramen-detail h3")
+
+let ratingDisplay= document.querySelector("#rating-display")
+
+let commentDisplay= document.querySelector("#comment-display")
+let form=document.querySelector("#new-ramen")
+
+document.addEventListener("DOMContentLoaded" ,()=>{
+    console.log("loaded")
+    fetchImages()
+    form.addEventListener("submit", (e)=>{
+        let body={}
+        e.preventDefault()
+        console.log(e.target)
+        let inputs=e.target.querySelectorAll(".input-text")
+        inputs.forEach(element => {
+            body[`${element.name}`]=element.value
+        });
+        console.log(body)
+
+        // create my image
+        let image= document.createElement("img")
+        image.setAttribute('src', `${body.image}`)
+        // image.setAttribute('id', `${body.id}`)
+        ramenDiv.appendChild(image)
+
+        form.reset()
+
+    })
+})
+
+
+const fetchImages=()=>{
+    let imageData;
+    fetch("http://localhost:3000/ramens")
+    .then(res=>res.json()).then(
+        data=> handleImage(data)
+    )
+    .catch(e=> console.log(e))
+
+    return imageData;
+}
+
+
+const handleImage=(data)=>{
+    data.map(ramen=>{
+        let image= document.createElement("img")
+        image.setAttribute('src', `${ramen.image}`)
+        image.setAttribute('id', `${ramen.id}`)
+        image.addEventListener("click", (e)=>{
+            console.log(e.target)
+            // let imageDetail=fetchImageDetail(parseInt(e.target.id))
+            // console.log(imageDetail)
+            fetch(`http://localhost:3000/ramens/2`)
+            .then(res=>res.json())
+            .then(data=>{
+                console.log(data)
+                imageName.innerHTML=data.name
+                imageRestaurant.innerHTML=data.restaurant
+                ratingDisplay.innerHTML=data.rating
+                commentDisplay.innerHTML=data.comment
+
+            }).catch(e=>console.log(e))
+        })
+        ramenDiv.appendChild(image);
+    })
+    
+}
+
